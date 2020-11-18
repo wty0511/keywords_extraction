daec0202cb544572ce621fb5e62951b45a759dad
wty0511 <15652198208@163.com>
2020-11-19 00:07:10 +0800

    update

--- process.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-data=pd.read_csv('./balance.csv')
-data = data.fillna(0)
-print(data)
-data.to_csv('./balance.csv')
--- ttt.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-data= pd.read_csv('./train.csv')
-data=data.fillna(0)
-data.to_csv('./train.csv',index=None)
