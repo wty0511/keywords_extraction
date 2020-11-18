93fee56261aa7bf6b9eaf66e98249f71a6f67a50
wty0511 <15652198208@163.com>
2020-11-19 00:02:24 +0800

    no data

--- feature.py
+++ feature.py
@@ -44,18 +44,15 @@ def get_candidate_word (title,content):
     jieba.load_userdict('./字典/网络流行新词.txt')
     jieba.load_userdict('./字典/显卡.txt')
 
-    ## 爬取漫漫看网站和百度热点上面的词条
+
     jieba.load_userdict('./字典/漫漫看_明星.txt')
     jieba.load_userdict('./字典/百度热点人物+手机+软件.txt')
     jieba.load_userdict('./字典/自定义词典.txt')
 
-    ## 实体名词抽取之后的结果 有一定的人工过滤
-    ## origin_zimu 这个只是把英文的组织名过滤出来
     jieba.load_userdict('./字典/person.txt')
     jieba.load_userdict('./字典/origin_zimu.txt')
 
-    ## 第一个是所有《》里面出现的实体名词
-    ## 后者是本地测试集的关键词加上了
+
     jieba.load_userdict('./字典/出现的作品名字.txt')
     jieba.load_userdict('./字典/val_keywords.txt')
     stopwords = [line.strip() for line in open('./stopword.txt', encoding='UTF-8').readlines()]
